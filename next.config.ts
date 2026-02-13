86d92238fea332aa5a72a892a62f6ce777e0e451
Chiccccccc <61212632+Chiccccccc@users.noreply.github.com>
2026-02-13 13:01:27 +0800

    Deploy as root site (remove basePath settings)

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const repo = "lab-website";
-
-// 判断是不是在 GitHub Actions 里构建
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-
 const nextConfig = {
   output: "export",
-  basePath: isGithubPages ? `/${repo}` : "",
-  assetPrefix: isGithubPages ? `/${repo}/` : "",
   images: { unoptimized: true },
 };
 
